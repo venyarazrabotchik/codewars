bbe90bda89b50bd565ab19185d68b4503bc47a12
venyarazrabotchik <venavena5879@gmail.com>
2024-06-21 14:10:18 +0300

    second commit

--- kata8/MergeTwoSortedArrays.go
+++ kata8/MergeTwoSortedArrays.go
@@ -1,5 +1,24 @@
 package kata8
 
+import (
+	"sort"
+)
+
 func MergeArrays(arr1, arr2 []int) []int {
-	return []int{}
+	merged := append(arr1, arr2...)
+
+	sort.Ints(merged)
+
+	var uniqueArray []int
+	if len(merged) > 0 {
+		uniqueArray = append(uniqueArray, merged[0])
+	}
+
+	for i := 1; i < len(merged); i++ {
+		if merged[i] != merged[i-1] {
+			uniqueArray = append(uniqueArray, merged[i])
+		}
+	}
+
+	return uniqueArray
 }
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hi")
-	kata8.MergeArrays()
+	fmt.Println(kata8.MergeArrays([]int{1, 2, 3, 4, 5}, []int{5, 6, 7, 8, 9}))
+	fmt.Println(kata8.MergeArrays([]int{1, 2, 3, 4, 5}, []int{4, -9, 5, 8}))
 
 }
